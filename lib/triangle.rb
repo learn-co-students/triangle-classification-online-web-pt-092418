719d9c864e9c3727034742f4edf3c86d1511055f
Carolyn Whelpley <carolynwhelpley@Jeffs-MacBook-Pro-3.local>
2018-11-01 11:05:51 -0400

    Interim checkin

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,30 @@
 class Triangle
-  # write code here
+ attr_accessor :side_one_length, :side_two_length, :side_three_length, :equilateral, :isosceles, :scalene
+
+    def initialize(side_one_length, side_two_length, side_three_length, equilateral, isosceles, scalene)
+      @side_one_length = side_one_length
+      @side_two_length = side_two_length
+      @side_three_length = side_three_length
+      @equilateral = equilateral
+      @isosceles = isosceles
+      @scalene = scalene
+    end
+    def kind
+    if size_one_length == size_two_length == size_three_length
+      return equilateral
+    elsif side_one_length == size_two_length != side_three_length
+      return isosceles
+    elsif side_one_length != side_two_length != side_three_length
+      return scalene
+    else 
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    end
+  class TriangleError < StandardError
+    def message 
+    end
+  end
 end

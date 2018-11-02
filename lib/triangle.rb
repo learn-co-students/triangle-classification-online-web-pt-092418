a76b3d9fe57bd313f7f333a316c3696f52be9f88
Anuj Panchal <anujpan1@gmail.com>
2018-11-02 19:22:08 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :side_one, :side_two, :side_three
+
+  def initialize(side_one, side_two, side_three)
+    @a = side_one
+    @b = side_two
+    @c = side_three
+    @shape = [side_one, side_two, side_three]
+  end
+
+  def trouble_triangle
+    @shape    
+  end
+
+  def kind
+    error_one = @a <= 0 || @b <= 0 || @c <= 0  
+    error_two = @a + @b <= @c || @b + @c <= @a || @a + @c <= @b 
+
+    if error_one || error_two
+      raise TriangleError      
+    elsif @shape.uniq.length == 1
+      :equilateral
+    elsif @shape.uniq.length == 2 
+      :isosceles
+    else
+      :scalene
+    end
+  end
+end
+
+  
+class TriangleError < StandardError
+  
 end

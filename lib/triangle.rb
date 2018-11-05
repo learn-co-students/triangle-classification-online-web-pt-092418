e697aab13b6f1180cf85dfe46badd65813d06f58
Carolyn Whelpley <carolynwhelpley@Jeffs-MacBook-Pro-3.local>
2018-11-04 20:39:24 -0500

    Interim checkin

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,25 +13,25 @@ class Triangle
 
   
       def kind
-    if (@side_one_length == @side_two_length) && (@side_one_length == @side_three_length)
+        if (@side_one_length == @side_two_length) && (@side_one_length == @side_three_length)
         :equilateral
     elsif (@side_one_length == @side_two_length) || (@side_one_length == @side_three_length) || (@side_two_length == @side_three_length)
        :isosceles
-    else
+    elsif ![@side_one_length, @side_two_length, @side_three_length].any? {|side| side < 1}
        :scalene
-    # else @side_one_length == 0
-    #   begin
-    #     raise TriangleError
-    #   rescue TriangleError => error
-    #     puts error.message
-    #   end
-    # end
+    else
+      begin
+         raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+        end
+    end
+      end
+  
+  
+    class TriangleError < StandardError
+      def message 
+        "Triangle::TriangleError"
       end
     end
-  
-    # class TriangleError < StandardError
-    #   def message 
-    #     "This is triangle inequality"
-    #   end
-    # end
 end

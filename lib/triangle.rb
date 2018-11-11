558161a17115e422579cb88e334a57c3eb5c49e6
Kevin J Gomez <kjgmez1@gmail.com>
2018-11-11 21:30:13 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,29 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  def initialize(one,two,three)
+    @side1=one
+    @side2=two
+    @side3=three
+  end
+  def kind
+    if side1 == side2 && side2 == side3
+      :equilateral
+    elsif side1 == side2 && side2 != side3
+      :isosceles
+    elsif side1 != side2 && side2 != side3
+      :scalene
+    else
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Enter the values of a valid triangel"
+    end
+  end
 end
